7d630c50223940519eb5af30a390c3e8bd7d7974
Arjun2007-IITB <arjunpawar2007ap@gmail.com>
2026-03-14 16:12:07 +0530

    planner.py
    Implemented a planning algorithm, to generate path.

--- PPC/racing_hkt_mac(si)/dist/participant/planner.py
+++ PPC/racing_hkt_mac(si)/dist/participant/planner.py
@@ -35,12 +35,19 @@ def plan(cones: list[dict]) -> list[dict]:
 
     # implement a planning algorithm to generate a path from the blue and yellow cones
 
-
-
-
-
-
-
+    if len(blue) == 0 or len(yellow) == 0:
+        return path
+        
+    for b_cone in blue 
+        distances = np.linalg.norm(yellow - b_cone, axis=1)
+        closest_idx = np.argmin(distances)
+        nearest_yellow = yellow[closest_idx]
+        
+        mid_x = (b_cone[0] + nearest_yellow[0]) / 2
+        mid_y = (b_cone[1] + nearest_yellow[1]) / 2
+        
+        path.append({"x": float(mid_x), "y": float(mid_y)})
+        path = sorted(path, key=lambda p: p["x"])
 
     return path
 

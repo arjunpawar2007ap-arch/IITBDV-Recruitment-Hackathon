b7641d6a3cbf621754c234732f948d1e8af0c5f9
Arjun2007-IITB <arjunpawar2007ap@gmail.com>
2026-03-21 14:34:11 +0530

    controller.py
    there was a mistake while uploading on GitHub which cause redundancy in the code

--- PPC/racing_hkt_mac(si)/dist/participant/controller.py
+++ PPC/racing_hkt_mac(si)/dist/participant/controller.py
@@ -74,53 +74,6 @@ def control(
     # 0.5 in the max steering angle in radians (about 28.6 degrees)
     return np.clip(steer, -0.5, 0.5)
 
-
-def throttle_algorithm(target_speed, current_speed, dt):
-    kp = 0.5 
-    error = target_speed - current_speed
-    
-    output = kp * error
-    # generate the output for throttle command
-    throttle = 0
-    brake = 0.0
-     if output > 0:
-        throttle = output
-    else:
-        brake = abs(output)
-    # clip throttle and brake to [0, 1]
-    return np.clip(output, 0.0, 1.0), np.clip(brake, 0.0, 1.0)
-
-def control(
-    path: list[dict],
-    state: dict,
-    cmd_feedback: dict,
-    step: int,
-) -> tuple[float, float, float]:
-    """
-    Generate throttle, steer, brake for the current timestep.
-    Called every 50ms during simulation.
-
-    Args:
-        path:         Your planned path (waypoints)
-        state:        Noisy vehicle state observation
-                        x, y        : position (m)
-                        yaw         : heading (rad)
-                        vx, vy      : velocity in body frame (m/s)
-                        yaw_rate    : (rad/s)
-        cmd_feedback: Last applied command with noise
-                        throttle, steer, brake
-        step:         Current simulation timestep index
-
-    Returns:
-        throttle  : float in [0.0, 1.0]   — 0=none, 1=full
-        steer     : float in [-0.5, 0.5]  — rad, neg=left
-        brake     : float in [0.0, 1.0]   — 0=none, 1=full
-    
-    Note: throttle and brake cannot both be > 0 simultaneously.
-    """
-    throttle = 0.0
-    steer    = 0.0
-    brake = 0.0
    
     # TODO: implement your controller here
     steer = steering(path, state)
